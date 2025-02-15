d8425ef38bf2578986af6da59032f05155608e87
Kaushal Singh <80886710+17kaushalsingh@users.noreply.github.com>
2025-02-15 10:25:35 +0900

    Add Lec5

---
+++ Lec5/bfsTraversal.cpp
@@ -0,0 +1,54 @@
+#include "../commons.cpp"
+
+void bfsTraversal(int startNode, vector<int> adj[], vector<bool> &isVisited, vector<int> &bfsTraversalAns)
+{
+    queue<int> q;
+    q.push(startNode);
+    isVisited[startNode] = true;
+
+    while (!q.empty())
+    {
+        int frontNode = q.front();
+        q.pop();
+        bfsTraversalAns.push_back(frontNode);
+
+        for (auto adjNode : adj[frontNode])
+        {
+            if (!isVisited[adjNode])
+            {
+                isVisited[adjNode] = true;
+                q.push(adjNode);
+            }
+        }
+    }
+}
+
+int main()
+{
+    // assume the graph is given
+    int n, m; // m nodes and n edges
+    cin >> n >> m;
+
+    vector<int> adj[n];
+
+    for (int i=0; i<m; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    vector<bool> isVisited(n, false);
+    vector<int> bfsTraversalAns;
+
+    for (int node=0; node<n; node++)
+    {
+        if (!isVisited[node]) bfsTraversal(node, adj, isVisited, bfsTraversalAns);
+    }
+
+    GraphUtils::printArray(bfsTraversalAns);
+
+    return 0;
+}
